c1c8958ae5c659fd90f31238b076f9ca80bd0e77
herehavetheseflowers <herehavetheseflowers@gmail.com>
2022-09-12 20:57:50 +0400

    bugfix

--- src/utils/Block.ts
+++ src/utils/Block.ts
@@ -126,7 +126,7 @@ class Block<P extends Record<string, any> = any> {
   private _render() {
     const fragment = this.render();
 
-    this._element?.innerHTML = '';
+    this._element!.innerHTML = '';
 
     this._element!.append(fragment);
 
